ef0557979563aad6222962382a82bb0c4f28982e
Vibhuti Maheshwari <vibhutimaheshwari03@gmail.com>
2021-11-20 12:05:45 +0530

    Basic React Pizza App

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import react from "react"
+
+const Pizza = () => {
+    return(
+        <h1>Hey its a Pizza App</h1>
+    )
+}
+
+export default Pizza
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import reactDom from "react-dom"
+
+import Pizza from "./App.js"
+
+reactDom.render(<Pizza/>,document.getElementById("root"))
